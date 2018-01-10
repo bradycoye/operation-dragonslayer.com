89c3d6aa0d075f8074a04f0e49be5425cfd06425
Jure Vrscaj <jure.vrscaj@gmail.com>
2018-01-10 14:53:22 +0100

    Remove implicit vote for yours founders

--- static/misc/rank.js
+++ static/misc/rank.js
@@ -58,7 +58,8 @@
                 NAME_TO_ID[name] = count;
                 ID_TO_VOTES[count] = 0;
                 ID_TO_NAME[count] = name;
-                NODES.push([NAME_TO_ID["Ryan X. Charles"], NAME_TO_ID["clemens"]]);
+                //NODES.push([NAME_TO_ID["Ryan X. Charles"], NAME_TO_ID["clemens"]]);
+                NODES.push([]);
                 count++;
             })
             
